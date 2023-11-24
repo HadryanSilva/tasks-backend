b822d95f6fd222c6221a8d6cd2ac6e29382fcb8d
hadryan <hadryan2001@gmail.com>
2023-11-24 18:05:32 -0300

    Nova tentativa

--- src/main/java/br/com/hadryan/tasksbackend/TasksBackendApplication.java
+++ src/main/java/br/com/hadryan/tasksbackend/TasksBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:application.properties")
 public class TasksBackendApplication {
 
 	public static void main(String[] args) {
